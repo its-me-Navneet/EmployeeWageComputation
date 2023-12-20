6a7412c6627e6e5da9f88fdbc83c39774c9b7285
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-20 12:44:09 +0530

    Implemented total wages for part time job, (UC6)

--- src/App.java
+++ src/App.java
@@ -11,17 +11,16 @@ public class App {
     }
 
     static int employeeWageForToday() {
-          int hourPresent =checkEmployeePresentOrAbsent(); 
-          System.out.println("Today you work for "+hourPresent);
-          return hourPresent * 20;
+        int hourPresent = checkEmployeePresentOrAbsent();
+        System.out.println("Today you work for " + hourPresent);
+        return hourPresent * 20;
     }
 
-   static int partTimeEmployeeWage() {
-         int WagePerHour = 10 ;
-         int hourPresent =checkEmployeePresentOrAbsent(); 
-          System.out.println("Today you work for "+hourPresent);
-          return hourPresent * WagePerHour;
-
+    static int partTimeEmployeeWage() {
+        int WagePerHour = 10;
+        int hourPresent = checkEmployeePresentOrAbsent();
+        System.out.println("Today you work for " + hourPresent);
+        return hourPresent * WagePerHour;
 
     }
 
@@ -30,60 +29,75 @@ public class App {
         return 20 * 8 * 20;
     }
 
+    static int wageForMonthPartTime() {
+
+        int day = 0;
+        int hr = 0;
+
+        while (day <= 20 && hr < 100) {
+            day++;
+            hr += checkEmployeePresentOrAbsent();
+        }
+        int Salary = hr * 20;
+        return Salary;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to calculating wage computation");
         System.out.println("************************************");
 
         Scanner sc = new Scanner(System.in);
-      
-         
+
         while (true) {
             System.out.println("Please Enter the Below Option");
             System.out.println("Option 1 Check present");
             System.out.println("Option 2 for calculating Today Salary ");
             System.out.println("Option 3 for calculating monthly salary ");
-            System.out.println("Option 4 for Exit ");
+            System.out.println("Option 4 for calculating monthly salary for part time ");
+            System.out.println("Option 5 for Exit ");
             System.out.println("******************************");
-              int option = sc.nextInt();
-                          sc.nextLine();
+            int option = sc.nextInt();
+            sc.nextLine();
             switch (option) {
                 case 1:
                     // For checking whether Employee is present or not
 
-                        int hour = checkEmployeePresentOrAbsent();
+                    int hour = checkEmployeePresentOrAbsent();
 
-                        if (hour == 0) {
-                            System.out.println("Absent");
-                        } else {
-                            System.out.println("Present");
-                        }
-                       break;
+                    if (hour == 0) {
+                        System.out.println("Absent");
+                    } else {
+                        System.out.println("Present");
+                    }
+                    break;
 
-                case 2: 
-                       // For Daily wage Computation
+                case 2:
+                    // For Daily wage Computation
 
-                        int wage = employeeWageForToday();
-                        System.out.println("And Income for today work is " + wage);
-                        break;
+                    int wage = employeeWageForToday();
+                    System.out.println("And Income for today work is " + wage);
+                    break;
                 case 3:
-                        // For calculating Monthly Wages   
-                        int totalWage = wagesForMonth();
-                        System.out.println("Total wages for a month is " + totalWage);
-                        break;
+                    // For calculating Monthly Wages
+                    int totalWage = wagesForMonth();
+                    System.out.println("Total wages for a month is " + totalWage);
+                    break;
                 case 4:
-                        System.out.println("Thank you for using this service"); 
-                        return ;
-                    
-                default:
-                       System.out.println("Please Enter valid option");
-                       break ;
-        }
-      System.out.println("******************************");
-        
-       
-    }
-   
+                    // For calculating Monthly Wages
+                    int Salary = wageForMonthPartTime();
+                    System.out.println("Total wages for a month is " + Salary);
+                    break; 
+                case 5:
+                    System.out.println("Thank you for using this service");
+                    return;
 
+                default:
+                    System.out.println("Please Enter valid option");
+                    break;
+            }
+            System.out.println("******************************");
+
+        }
 
     }
 }
